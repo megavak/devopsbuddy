30dcb002197ba7b5c8c52ea6fd5d31b9bd880b39
Izu <megavak@gmail.com>
2017-11-26 21:48:42 +0200

    initial commit

---
+++ src/main/java/com/megavak/DevsbuddyopsApplication.java
@@ -0,0 +1,12 @@
+package com.megavak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsbuddyopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsbuddyopsApplication.class, args);
+	}
+}
